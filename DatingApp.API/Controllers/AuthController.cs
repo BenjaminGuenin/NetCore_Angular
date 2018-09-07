c56c28acedd8685ad28d4ed0b4f697de95c32f19
Light_Celestial <lightcelestial55555@gmail.com>
2018-09-07 15:14:28 +0200

    33 - Creating the Login method in the API

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,7 +3,12 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,9 +16,11 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
@@ -29,6 +36,46 @@ namespace DatingApp.API.Controllers
 
             //return CreatedAtRoute(userToCreate.GetUser());
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+
+            // Step 1 - Checking that the username and password match what is stored in the database.
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // **** Build up a token that we will return to the user. ****
+
+            // Step 2 - Our token contains two claims, user id and username
+            var claims = new []
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username),
+            };
+
+            // Step 3 - In order to make sure our token is a valid token when it comes back, the server needs to sign this token
+
+            // Step 3.1 - Creating Security Key
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            // Generate some signing credentials using the key and encrypting this key.
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // Step 4 - We actually start creating the Token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHandler.WriteToken(token) });
         }
     }
 }
