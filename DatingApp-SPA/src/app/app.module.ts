c26501dd7af2df0aed098d940fbd52b1ff452873
BenjaminGuenin <benjamin.guenin.pro@protonmail.com>
2018-09-07 18:31:37 +0200

    38 - Introduction to Angular Template forms

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./components/value/value.component";
-import { NavComponent } from './components/nav/nav.component';
+import { NavComponent } from "./components/nav/nav.component";
 
 @NgModule({
   declarations: [AppComponent, ValueComponent, NavComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
