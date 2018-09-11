b04028c6b74052abb097e5c78a7b04f9278494fa
BenjaminGuenin <benjamin.guenin.pro@protonmail.com>
2018-09-11 11:25:36 +0200

    Fix Handling Error

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -42,10 +42,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no!");
-
             try
             {
+                throw new Exception("Computer says no!");
                 // Step 1 - Checking that the username and password match what is stored in the database.
                 var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
                 if (userFromRepo == null)
