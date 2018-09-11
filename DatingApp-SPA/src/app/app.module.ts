b691692aef2a5e9e1d7c30fc8d5f98900174d4a6
BenjaminGuenin <benjamin.guenin.pro@protonmail.com>
2018-09-11 09:43:45 +0200

    42 - Add Register Component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ValueComponent } from "./components/value/value.component";
+
 import { NavComponent } from "./components/nav/nav.component";
-import { AuthService } from './_services/auth.service';
+import { HomeComponent } from "./components/home/home.component";
+import { RegisterComponent } from "./components/register/register.component";
+
+import { AuthService } from "./_services/auth.service";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [
-    AuthService
-  ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ DatingApp-SPA/src/app/components/register.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RegisterDirective } from './register.directive';
+
+describe('Directive: Register', () => {
+  it('should create an instance', () => {
+    const directive = new RegisterDirective();
+    expect(directive).toBeTruthy();
+  });
+});
