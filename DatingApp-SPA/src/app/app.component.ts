7d2b6b2d916c03a09ae80855b0afb95852e621c6
BenjaminGuenin <benjamin.guenin.pro@protonmail.com>
2018-09-13 18:13:07 +0200

    56 - Using the Angular JWT Library to decode tokens

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: "app-root",
@@ -6,7 +8,13 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "app";
+  jwtHelperService = new JwtHelperService();
 
-  public test() {}
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token)
+      this.authService.decodedToken = this.jwtHelperService.decodeToken(token)
+  }
 }
