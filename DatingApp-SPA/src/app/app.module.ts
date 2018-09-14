430af36f7325e55fce063ae0d5f999e3eede8cf6
BenjaminGuenin <benjamin.guenin.pro@protonmail.com>
2018-09-14 10:10:53 +0200

    56 - Adding Ngx dropdown menu

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from "./app.component";
-
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from "./components/home/home.component";
 import { RegisterComponent } from "./components/register/register.component";
@@ -16,7 +16,12 @@ import { AlertifyService } from './_services/alertify.service';
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
 
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [
     AuthService,
     ErrorInterceptorProvider,
